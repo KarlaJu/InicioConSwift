89f6e89a71bfb930472fd5c30cb987241dce277e
karlaJu <karla@makingdevs.com>
2018-05-16 14:46:13 -0500

    Subiendo ejemplo de hola mundo con Swift

--- NombreconBoton/ViewController.swift
+++ NombreconBoton/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var saludo: UILabel!
+    @IBOutlet weak var nombre: UITextField!
+    
+    @IBAction func presionar(_ sender: Any) {
+        saludo.text = "Hola \(nombre.text!)!"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
